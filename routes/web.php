aed991ddec408e7d11e1e218210f8c3cc94e0563
Sweet Ang <sflang00005@usep.edu.ph>
2024-09-16 00:09:56 +0800

    1st

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,25 @@
-<?php
+    <?php
 
-use Illuminate\Support\Facades\Route;
+    use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::middleware([
+        'auth:sanctum',
+        config('jetstream.auth_session'),
+        'verified',
+    ])->group(function () {
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+    });
+    // VIEWPROJECT SWEET
+    Route::get('/viewproject1', function () {
+        return view('layouts.Projects.viewproject1');
+    });
+    Route::get('/viewproject2', function () {
+        return view('layouts.Projects.viewproject2');
+    });
