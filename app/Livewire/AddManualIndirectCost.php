ba473adcb118086ec1ba2ac6efaf65cafc3a4574
JohnnySons <lorjohn143@gmail.com>
2024-10-27 00:35:25 +0800

    Indirect Cost

---
+++ app/Livewire/AddManualIndirectCost.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\IndirectCost;
+use Livewire\Component;
+
+class AddManualIndirectCost extends Component
+{
+    public $indirect_costs = [];
+    public $pow_id;
+
+    public function mount($pow_id): void
+    {
+        $this->pow_id = $pow_id;
+    }
+
+    public function addCost()
+    {
+        $this->indirect_costs[] = ['description' => '', 'amount' => 0];
+    }
+
+    public function removeCost($index)
+    {
+        unset($this->indirect_costs[$index]);
+        $this->indirect_costs = array_values($this->indirect_costs);
+    }
+
+    public function saveIndirectCosts()
+    {
+        // Validation
+        $this->validate([
+            'indirect_costs.*.description' => 'required|string|max:255',
+            'indirect_costs.*.amount' => 'required|numeric|min:0',
+        ]);
+
+        foreach ($this->indirect_costs as $cost) {
+            // Include pow_id when creating each IndirectCost
+            IndirectCost::create([
+                'pow_id' => $this->pow_id, // Adding pow_id to each entry
+                'description' => $cost['description'],
+                'amount' => $cost['amount'],
+            ]);
+        }
+
+        // Reset the indirect costs array
+        $this->reset('indirect_costs');
+
+        // Dispatch an event to indicate the costs have been saved
+        $this->dispatch('indirectCostsSaved');
+    }
+
+    public function render()
+    {
+        return view('livewire.add-manual-indirect-cost');
+    }
+}
