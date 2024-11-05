0c2f776fa58198e358382a9845a4810556f126af
JohnnySons <lorjohn143@gmail.com>
2024-11-05 22:44:39 +0800

    other direct costs

---
+++ app/Livewire/OtherDirectCostTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class OtherDirectCostTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.other-direct-cost-table');
+    }
+}
