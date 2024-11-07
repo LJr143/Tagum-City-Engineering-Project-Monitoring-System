8b9368063baa5c7fac10c84b3d20909ca852f9d9
Sweet Ang <sflang00005@usep.edu.ph>
2024-11-07 22:45:21 +0800

    add project modal and filter responsive changes

--- resources/views/livewire/project-filter.blade.php
+++ resources/views/livewire/project-filter.blade.php
@@ -55,7 +55,7 @@
                     type="date"
                     id="start-date"
                     x-model="startDate"
-                    class="mt-1 block h-8 w-full border-gray-300 rounded-md shadow-sm focus:ring-green-500 focus:border-green-500 sm:text-xs"
+                    class="mt-1 block h-8 w-full border-gray-300 rounded-md shadow-sm focus:ring-green-500 focus:border-green-500 text-xs sm:text-[10px] md:text-xs"
                     @change="$wire.filterProjects()">
             </div>
 
@@ -66,7 +66,7 @@
                     type="date"
                     id="end-date"
                     x-model="endDate"
-                    class="mt-1 block h-8 w-full border-gray-300 rounded-md shadow-sm focus:ring-green-500 focus:border-green-500 sm:text-xs"
+                    class="mt-1 block h-8 w-full border-gray-300 rounded-md shadow-sm focus:ring-green-500 focus:border-green-500 text-xs sm:text-[10px] md:text-xs"
                     @change="$wire.filterProjects()">
             </div>
 
