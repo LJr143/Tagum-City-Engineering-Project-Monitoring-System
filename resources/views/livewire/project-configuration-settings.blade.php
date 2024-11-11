9443f191e876577acca229c15fa19b6cd7f65bea
Keyem04 <krstnmvrgs04@gmail.com>
2024-11-11 21:27:25 +0800

    route

--- resources/views/livewire/project-configuration-settings.blade.php
+++ resources/views/livewire/project-configuration-settings.blade.php
@@ -2,7 +2,7 @@
     <div class="flex justify-end">
         <div class="relative ml-2 w-full">
             <button @click="open = true"
-                    class="flex bg-green-500 text-white text-xs px-6 py-2 rounded shadow-md hover:bg-green-600 focus:outline-none w-[310px] sm:w-auto">
+                    class="text-xs bg-green-500 text-white px-4 py-2 rounded flex items-center space-x-2 shadow-md hover:bg-green-600 focus:outline-none ">
                 Set Up Project Progress Indicator
 
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
