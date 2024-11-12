3c92d8e583796d2a0c1de1b22f15e78505eaeecf
Keyem04 <krstnmvrgs04@gmail.com>
2024-11-13 03:31:19 +0800

    route

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -8,25 +8,28 @@
         </svg>
     </button>
 
+    <div class="flex space-x-4 sm:pl-2 pr-2">
+        <img alt="Seal 1" src="http://127.0.0.1:8000/storage/pmsAssets/logo_img.png" class="w-[130px]">
+    </div>
     <!-- Search Bar -->
 
-    <div class="relative w-full max-w-[550px] bg-white rounded-[10px] shadow-[0px_1px_2px_-1px_rgba(0,0,0,0.2)]">
-        <input class="w-full py-2 pl-4 pr-10 rounded-[10px] focus:outline-none focus:ring-2 focus:ring-green-500 sm:placeholder:text-sm placeholder:text-xs placeholder:opacity-75 outline-none border-none bg-transparent z-10"
-               name="search"
-               id="search"
-               placeholder="Search here"
-               type="text"/>
+    <!--    <div class="relative w-full max-w-[550px] bg-white rounded-[10px] shadow-[0px_1px_2px_-1px_rgba(0,0,0,0.2)]">
+            <input class="w-full py-2 pl-4 pr-10 rounded-[10px] focus:outline-none focus:ring-2 focus:ring-green-500 sm:placeholder:text-sm placeholder:text-xs placeholder:opacity-75 outline-none border-none bg-transparent z-10"
+                   name="search"
+                   id="search"
+                   placeholder="Search here"
+                   type="text"/>
 
-        <svg class="text-gray-500 absolute top-3 right-3" width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M23.7871 22.7761L17.9548 16.9437C19.5193 15.145 20.4665 12.7982 20.4665 10.2333C20.4665 4.58714 15.8741 0 10.2333 0C4.58714 0 0 4.59246 0 10.2333C0 15.8741 4.59246 20.4665 10.2333 20.4665C12.7982 20.4665 15.145 19.5193 16.9437 17.9548L22.7761 23.7871C22.9144 23.9255 23.1007 24 23.2816 24C23.4625 24 23.6488 23.9308 23.7871 23.7871C24.0639 23.5104 24.0639 23.0528 23.7871 22.7761ZM1.43149 10.2333C1.43149 5.38004 5.38004 1.43681 10.2279 1.43681C15.0812 1.43681 19.0244 5.38537 19.0244 10.2333C19.0244 15.0812 15.0812 19.035 10.2279 19.035C5.38004 19.035 1.43149 15.0865 1.43149 10.2333Z" fill="#A4A4A4" />
-        </svg>
-    </div>
+            <svg class="text-gray-500 absolute top-3 right-3" width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M23.7871 22.7761L17.9548 16.9437C19.5193 15.145 20.4665 12.7982 20.4665 10.2333C20.4665 4.58714 15.8741 0 10.2333 0C4.58714 0 0 4.59246 0 10.2333C0 15.8741 4.59246 20.4665 10.2333 20.4665C12.7982 20.4665 15.145 19.5193 16.9437 17.9548L22.7761 23.7871C22.9144 23.9255 23.1007 24 23.2816 24C23.4625 24 23.6488 23.9308 23.7871 23.7871C24.0639 23.5104 24.0639 23.0528 23.7871 22.7761ZM1.43149 10.2333C1.43149 5.38004 5.38004 1.43681 10.2279 1.43681C15.0812 1.43681 19.0244 5.38537 19.0244 10.2333C19.0244 15.0812 15.0812 19.035 10.2279 19.035C5.38004 19.035 1.43149 15.0865 1.43149 10.2333Z" fill="#A4A4A4" />
+            </svg>
+        </div>
 
+    -->
 
+        <div class="flex items-center justify-end">
 
-    <div class="flex items-center justify-end">
-
-        <!-- Notification Bell -->
+            <!-- Notification Bell -->
         <livewire:notification-bell/>
 
         <!-- Profile -->
