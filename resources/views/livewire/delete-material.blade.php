806118467acb08e6002a38591b6d1b1cc37227ea
Keyem04 <krstnmvrgs04@gmail.com>
2024-12-03 18:51:30 +0800

    edit and delete material

--- resources/views/livewire/delete-material.blade.php
+++ resources/views/livewire/delete-material.blade.php
@@ -1,17 +1,17 @@
 <div class="flex text-left" x-data="{ open: false }" x-cloak @open-edit-modal.window="open = true" @material-edited.window="open = false">
     <div class="flex justify-content-center align-items-center">
         <div class="relative">
-            <button @click="open = true" class="flex  text-white text-xs rounded focus:outline-none">
-                <svg width="18" height="18" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="ml-2">
-                    <path d="M10 10V16" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M14 10V16" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M18 6V18C18 19.1046 17.1046 20 16 20H8C6.89543 20 6 19.1046 6 18V6" stroke="red"
-                          stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M4 6H20" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M15 6V5C15 3.89543 14.1046 3 13 3H11C9.89543 3 9 3.89543 9 5V6" stroke="red" stroke-width="1.5"
-                          stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
 
+            <button @click="open = true" class="flex bg-red-500 text-white text-xs px-4 py-2 rounded shadow-md hover:bg-red-600 focus:outline-none">
+                <svg width="18" height="18" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="ml-2">
+                    <path d="M10 10V16" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M14 10V16" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M18 6V18C18 19.1046 17.1046 20 16 20H8C6.89543 20 6 19.1046 6 18V6" stroke="white"
+                          stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M4 6H20" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M15 6V5C15 3.89543 14.1046 3 13 3H11C9.89543 3 9 3.89543 9 5V6" stroke="white"
+                          stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
             </button>
         </div>
     </div>
@@ -36,26 +36,20 @@
             x-transition:leave="transition ease-in duration-200"
             x-transition:leave-start="opacity-100 transform scale-100"
             x-transition:leave-end="opacity-0 transform scale-90"
-            class="bg-white w-full max-w-[700px] p-6 rounded-lg relative"
+            class="bg-white w-full max-w-md sm:max-w-md shadow-lg p-6 rounded-lg mx-4 sm:mx-auto"
         >
-            <!-- Close Button (X) -->
-            <button @click="open = false" class="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
-
             <div class="flex items-center mb-2">
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-2">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.4845 2.49512C9.15808 1.32845 10.842 1.32845 11.5156 2.49512L17.7943 13.3701C18.4678 14.5368 17.6259 15.9951 16.2787 15.9951H3.72136C2.37421 15.9951 1.53224 14.5368 2.20582 13.3701L8.4845 2.49512ZM10 5.00012C10.4142 5.00012 10.75 5.33591 10.75 5.75012V9.25012C10.75 9.66434 10.4142 10.0001 10 10.0001C9.58579 10.0001 9.25 9.66434 9.25 9.25012L9.25 5.75012C9.25 5.33591 9.58579 5.00012 10 5.00012ZM10 14.0001C10.5523 14.0001 11 13.5524 11 13.0001C11 12.4478 10.5523 12.0001 10 12.0001C9.44772 12.0001 9 12.4478 9 13.0001C9 13.5524 9.44772 14.0001 10 14.0001Z" fill="#CA383A"/>
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"
+                     class="mr-2">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                          d="M8.4845 2.49512C9.15808 1.32845 10.842 1.32845 11.5156 2.49512L17.7943 13.3701C18.4678 14.5368 17.6259 15.9951 16.2787 15.9951H3.72136C2.37421 15.9951 1.53224 14.5368 2.20582 13.3701L8.4845 2.49512ZM10 5.00012C10.4142 5.00012 10.75 5.33591 10.75 5.75012V9.25012C10.75 9.66434 10.4142 10.0001 10 10.0001C9.58579 10.0001 9.25 9.66434 9.25 9.25012L9.25 5.75012C9.25 5.33591 9.58579 5.00012 10 5.00012ZM10 14.0001C10.5523 14.0001 11 13.5524 11 13.0001C11 12.4478 10.5523 12.0001 10 12.0001C9.44772 12.0001 9 12.4478 9 13.0001C9 13.5524 9.44772 14.0001 10 14.0001Z"
+                          fill="#CA383A"/>
                 </svg>
-                <h2 class="text-sm font-semibold text-red-500">Delete Project</h2>
+                <h2 class="text-sm font-semibold text-red-500">Delete Material</h2>
             </div>
-            <p class="text-xs mb-4 ml-7">Are you sure you want to remove this item? This process cannot be
-                undone.</p>
+            <p class="text-xs mb-4 ml-7 whitespace-normal">Are you sure you want to remove this item? This process cannot be undone.</p>
+
             <form wire:submit.prevent="confirmDelete" class="text-xs">
-
-
                 <!-- Action Buttons -->
                 <div class="flex justify-end">
                     <button type="button" id="delete-cancel-button" @click="open = false"
@@ -67,6 +61,8 @@
                         Delete
                     </button>
                 </div>
+
+
             </form>
         </div>
     </div>
--- resources/views/livewire/edit-material.blade.php
+++ resources/views/livewire/edit-material.blade.php
@@ -1,11 +1,13 @@
 <div class="flex text-left" x-data="{ open: false }" x-cloak @open-edit-modal.window="open = true" @material-edited.window="open = false">
     <div class="flex justify-end">
         <div class="relative">
-            <button @click="open = true" class="flex text-white text-xs rounded focus:outline-none">
-                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M21 7L17 3L14 6L8 12V16H12L18 10L21 7Z" stroke="green" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M14 6L18 10" stroke="green" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M10 4H4V20H20V14" stroke="green" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+
+            <button @click="open = true" class="flex bg-green-500 text-white text-xs px-4 py-2 rounded shadow-md hover:bg-green-600 focus:outline-none">
+
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="ml-2">
+                    <path d="M21 7L17 3L14 6L8 12V16H12L18 10L21 7Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M14 6L18 10" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M10 4H4V20H20V14" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
 
             </button>
@@ -32,51 +34,68 @@
             x-transition:leave="transition ease-in duration-200"
             x-transition:leave-start="opacity-100 transform scale-100"
             x-transition:leave-end="opacity-0 transform scale-90"
-            class="bg-white w-full max-w-[700px] p-6 rounded-lg relative"
+            class="bg-white p-4 md:p-6 rounded-lg shadow-lg w-full max-w-md md:max-w-[700px] overflow-y-auto max-h-full md:max-h-screen mx-4 sm:mx-8"
         >
-            <!-- Close Button (X) -->
-            <button @click="open = false" class="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
 
-            <h2 class="text-lg font-bold mb-4">Edit Project</h2>
+            <div class="flex justify-between items-center mb-4 border-b border-gray-300 pb-2">
+                <h2 class="text-sm font-bold text-left w-full sm:w-auto">Edit Material</h2>
+
+                <!-- Close Button (X) -->
+                <button @click="open = false" class="text-gray-500 hover:text-gray-700">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+
+
             <form wire:submit.prevent="submit" class="text-xs">
+                <div class="grid gap-3">
+                    <div>
+                        <label for="item_description" class="block text-xs font-medium mb-1">Item Description</label>
+                        <textarea id="item_description" wire:model="item_description"
+                                  class="w-full h-[60px] md:h-auto  py-2 text-xs border border-gray-300 focus:ring-green-500 focus:border-green-500 rounded resize-none"
+                                  style="vertical-align: top;" placeholder="Enter item description..."></textarea>
+                        @error('item_description') <span class="text-red-500 text-xs">{{ $message }}</span> @enderror
+                    </div>
 
-                <div class="flex flex-col space-y-2">
-                    <label for="item_description" class="block text-gray-700 text-xs">Item Description</label>
-                    <input type="text" id="item_description" wire:model="item_description"
-                           class="mt-1 block w-full p-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-xs">
-                    @error('item_description') <span class="text-red-500 text-xs">{{ $message }}</span> @enderror
-                </div>
-
-                <div class="flex flex-col space-y-1">
-                    <label for="quantity" class="block text-gray-700 text-xs">Quantity</label>
+                <div>
+                    <label for="quantity" class="block text-xs font-medium mb-1">Quantity</label>
                     <input type="text" id="quantity" wire:model="quantity"
-                           class="mt-1 block w-full p-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-xs">
+                           class="w-full px-3 py-2 text-xs border border-gray-300 focus:ring-green-500 focus:border-green-500 rounded">
                     @error('quantity') <span class="text-red-500 text-xs">{{ $message }}</span> @enderror
                 </div>
 
-                <div class="flex flex-col space-y-1">
-                    <label for="estimated_unit_cost" class="block text-gray-700 text-xs">Unit Cost</label>
+                <div>
+                    <label for="estimated_unit_cost" class="block text-xs font-medium mb-1">Unit Cost</label>
                     <input type="text" id="estimated_unit_Cost" wire:model="estimated_unit_cost"
-                           class="mt-1 block w-full p-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-xs"
+                           class="w-full px-3  text-xs border border-gray-300 focus:ring-green-500 focus:border-green-500 rounded"
                            readonly>
                     @error('estimated_unit_cost') <span class="text-red-500 text-xs">{{ $message }}</span> @enderror
                 </div>
 
-                <div class="flex flex-col space-y-1">
-                    <label for="estimated_cost" class="block text-gray-700 text-xs">Total Cost</label>
+                <div>
+                    <label for="estimated_cost" class="block text-xs font-medium mb-1">Total Cost</label>
                     <input type="text" id="estimated_cost" wire:model="estimated_cost"
-                           class="mt-1 block w-full p-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-xs" readonly>
+                           class="w-full px-3  text-xs border border-gray-300 focus:ring-green-500 focus:border-green-500 rounded" readonly>
                     @error('estimated_cost') <span class="text-red-500 text-xs">{{ $message }}</span> @enderror
                 </div>
-                <!-- Action Buttons -->
-                <div class="flex justify-end space-x-2 mt-4">
-                    <button type="button" @click="open = false" class="bg-gray-300 text-gray-700 rounded-md px-4 py-2 text-xs">Cancel</button>
-                    <button type="submit" class="bg-green-700 text-white rounded-md px-4 py-2 text-xs hover:bg-green-800">Save Changes</button>
                 </div>
+
+                <!-- Action Buttons -->
+                <div class="mt-6 flex justify-end space-x-2">
+                    <button @click="open = false"
+                            type="button"
+                            class="bg-gray-200 text-gray-700 px-4 py-2 rounded shadow-md hover:bg-gray-400 text-xs w-1/2 focus:ring-2 focus:ring-green-500 focus:border-green-500">
+                        Cancel
+                    </button>
+
+                    <button type="submit"
+                            class="bg-green-500 text-white px-4 py-2 rounded shadow-md hover:bg-green-600 text-xs w-1/2 focus:ring-2 focus:ring-green-500 focus:border-green-500">
+                        Save Changes
+                    </button>
+                </div>
+
+
+
             </form>
         </div>
     </div>
