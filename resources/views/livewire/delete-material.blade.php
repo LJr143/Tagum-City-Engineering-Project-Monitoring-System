70f015f25800f8be3a82e6330b838a7ac35349e7
JohnnySons <lorjohn143@gmail.com>
2024-12-04 20:17:52 +0800

    Add ons

--- resources/views/livewire/delete-material.blade.php
+++ resources/views/livewire/delete-material.blade.php
@@ -2,14 +2,14 @@
     <div class="flex justify-content-center align-items-center">
         <div class="relative">
 
-            <button @click="open = true" class="flex bg-red-500 text-white text-xs px-4 py-2 rounded shadow-md hover:bg-red-600 focus:outline-none">
+            <button @click="open = true" class="flex text-white text-xs rounded  focus:outline-none">
                 <svg width="18" height="18" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="ml-2">
-                    <path d="M10 10V16" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M14 10V16" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M18 6V18C18 19.1046 17.1046 20 16 20H8C6.89543 20 6 19.1046 6 18V6" stroke="white"
+                    <path d="M10 10V16" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M14 10V16" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M18 6V18C18 19.1046 17.1046 20 16 20H8C6.89543 20 6 19.1046 6 18V6" stroke="red"
                           stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M4 6H20" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M15 6V5C15 3.89543 14.1046 3 13 3H11C9.89543 3 9 3.89543 9 5V6" stroke="white"
+                    <path d="M4 6H20" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M15 6V5C15 3.89543 14.1046 3 13 3H11C9.89543 3 9 3.89543 9 5V6" stroke="red"
                           stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </button>
